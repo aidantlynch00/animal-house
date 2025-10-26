72fc2dae0b75fa29e23a89857a7d045d4051d899
Leann Kuchler <lxkuchler@gmail.com>
2025-10-26 12:52:20 -0400

    updated animal render placeholder to be universal and hold all mechanic-relevant information

--- src/animals.js
+++ src/animals.js
@@ -24,19 +24,20 @@ class Animal {
         this.stars = stars;
     }
 
-    render(x, y) { }
+    render(x, y) { 
+        fill(255);
+        noStroke();
+        text(`${this.name}`, x, y);
+        text(`Cuteness: ${this.cuteness}`, x, y+20);
+        text(`Cash: ${this.cash}`, x, y+40);
+        text(`Stars: ${this.stars}`, x, y+60);
+        text(`Ability: ${this.ability}`, x, y+80);
+    }
 }
 
 class Cow extends Animal {
     constructor() {
         super("cow", "Cow", false, 0, 2, 0, Ability.Trouble, 0);
-    }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Cow", x, y);
-        text("2", x, y+50);
     }
 }
 
@@ -44,25 +45,11 @@ class Hamster extends Animal {
     constructor() {
         super("hamster", "Hamster", true, 2, 1, 0, Ability.None, 0);
     }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Hamster", x, y);
-        text("1", x, y+50);
-    }
 }
 
 class Fish extends Animal {
     constructor() {
         super("fish", "Fish", true, 3, 0, 1, Ability.None, 0);
-    }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Fish", x, y);
-        text("0", x, y+50);
     }
 }
 
@@ -70,25 +57,11 @@ class Golden extends Animal {
     constructor() {
         super("golden", "Golden Retriever", true, 3, 0, 0, Ability.Fetch, 0);
     }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Golden Retriever", x, y);
-        text("0", x, y+50);
-    }
 }
 
 class Snake extends Animal {
     constructor() {
         super("snake", "Snake", true, 3, 4, 0, Ability.Trouble, 0);
-    }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Snake", x, y);
-        text("4", x, y+50);
     }
 }
 
@@ -96,37 +69,16 @@ class Horse extends Animal {
     constructor() {
         super("horse", "Horse", true, 4, 0, 0, Ability.Boot, 0);
     }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Horse", x, y);
-        text("0", x, y+50);
-    }
 }
 
 class Pig extends Animal {
     constructor() {
         super("pig", "Pig", true, 4, -1, 2, Ability.None, 0);
     }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Pig", x, y);
-        text("-1", x, y+50);
-    }
 }
 class BlackCat extends Animal {
     constructor() {
         super("black_cat", "Black Cat", true, 4, 2, 0, Ability.Peek, 0);
-    }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Black Cat", x, y);
-        text("2", x, y+50);
     }
 }
 
@@ -134,25 +86,11 @@ class Turtle extends Animal {
     constructor() {
         super("turtle", "Turtle", true, 4, 1, 0, Ability.ReduceTrouble, 0);
     }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Turtle", x, y);
-        text("1", x, y+50);
-    }
 }
 
 class Parrot extends Animal {
     constructor() {
         super("parrot", "Parrot", true, 5, 3, 2, Ability.Trouble, 0);
-    }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Parrot", x, y);
-        text("3", x, y+50);
     }
 }
 
@@ -160,25 +98,11 @@ class Mouse extends Animal {
     constructor() {
         super("mouse", "Mouse", true, 5, 0, -1, Ability.Full, 0);
     }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Mouse", x, y);
-        text("0", x, y+50);
-    }
 }
 
 class Rabbit extends Animal {
     constructor() {
         super("rabbit", "Rabbit", true, 5, 4, -1, Ability.None, 0);
-    }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Rabbit", x, y);
-        text("4", x, y+50);
     }
 }
 
@@ -186,25 +110,11 @@ class Sheep extends Animal {
     constructor() {
         super("sheep", "Sheep", true, 5, 3, 0, Ability.Guest, 0);
     }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Sheep", x, y);
-        text("3", x, y+50);
-    }
 }
 
 class Bee extends Animal {
     constructor() {
         super("bee", "Honey Bee", true, 7, 0, 0, Ability.Combo, 0);
-    }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Honey Bee", x, y);
-        text("0", x, y+50);
     }
 }
 
@@ -212,24 +122,10 @@ class Chicken extends Animal {
     constructor() {
         super("chicken", "Chicken", true, 9, 0, 3, Ability.None, 0);
     }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Chicken", x, y);
-        text("0", x, y+50);
-    }
 }
 
 class Alpaca extends Animal {
     constructor() {
         super("alpaca", "Alpaca", true, 40, 0, 0, Ability.None, 1);
-    }
-
-    render(x, y) {
-        fill(255);
-        noStroke();
-        text("Alpaca", x, y);
-        text("1 star", x, y+50);
     }
 }
