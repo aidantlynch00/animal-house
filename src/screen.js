4f6445d73d591e50bfb4035ad91af7147b545fa1
Leann Kuchler <lxkuchler@gmail.com>
2025-10-25 21:30:24 -0400

    ECONOMY SCREEN: recreated the economy screen grid layout and render text for economy stats. Also created outline for additional screens.

--- src/screen.js
+++ src/screen.js
@@ -9,6 +9,12 @@ class Screen {
     handleAction(action) {}
 }
 
+/*
+HOUSE SCREEN
+displays the house
+
+TODO:    
+*/
 class HouseScreen extends Screen {
     constructor(state) {
         super(state);
@@ -41,7 +47,8 @@ class HouseScreen extends Screen {
         rect(renderX, renderY, renderWidth, renderHeight, roundedRadius);
         renderX += renderWidth;
 
-        for (let animal of state.deck) {
+        if(state.party.length > 0){
+            for (let animal of state.party) {
             //set boundaries
             if(renderX >= width) {
                 renderX = x;
@@ -56,7 +63,9 @@ class HouseScreen extends Screen {
             animal.render(renderX+padding, renderY+padding);
             count += 1;
             renderX += renderWidth;
+            }
         }
+        
     }
 
     handleAction(action) {
@@ -64,6 +73,12 @@ class HouseScreen extends Screen {
     }
 }
 
+/*
+SHOP SCREEN
+displays the shop
+
+TODO:    
+*/
 class ShopScreen extends Screen {
     //the item name
     //the item ability if there are any
@@ -84,4 +99,108 @@ class ShopScreen extends Screen {
     handleAction(action) {
 
     }
+}
+
+/*
+ROLEDEX SCREEN
+displays your deck
+
+TODO:    
+*/
+class RoledexScreen extends Screen {
+
+}
+
+//Displays the Name of the Main Screen (shop, house, roledex) that is on display
+class TitleScreen extends Screen {
+
+}
+
+//Displays important information
+class TextScreen extends Screen {
+
+}
+
+//Displays turns left, current cuteness, and current cash
+class EconomyScreen extends Screen {
+    constructor(state) {
+        super(state);
+    }
+
+    render(x, y, width, height, padding, roundedRadius) {
+        this.renderEconomy(x, y, width, height, padding, roundedRadius);
+    }
+
+    renderEconomy(x, y, width, height, padding, roundedRadius) {
+        let renderHeight = height/3;
+        let renderWidth = width/4;
+
+        //Turns
+        stroke(255);
+        fill(0);
+        rect(x+padding, y+padding, renderWidth, renderHeight-(padding), roundedRadius);
+        rect(x+padding*2+renderWidth, y+padding, renderWidth*3-padding*3, renderHeight-(padding), roundedRadius);
+        noStroke();
+        fill(255);
+        text("Turns", x+padding+5, y+padding+20);
+        text(`${state.turnsLeft}`, x+padding*2+renderWidth+5, y+padding+20);
+
+        //Cuteness
+        stroke(255);
+        fill(0);
+        rect(x+padding, y+renderHeight+padding, renderWidth, renderHeight-(padding), roundedRadius);
+        rect(x+padding*2+renderWidth, y+renderHeight+padding, renderWidth*3-padding*3, renderHeight-(padding), roundedRadius);
+        noStroke();
+        fill(255);
+        text("Cuteness", x+padding+5, y+renderHeight+padding+20);
+        text(`${state.cuteness} / ${CUTE_MAX}`, x+padding*2+renderWidth+5, y+renderHeight+padding+20);
+
+        //Cash
+        stroke(255);
+        fill(0);
+        rect(x+padding, y+renderHeight*2+(padding), renderWidth, renderHeight-padding, roundedRadius);
+        rect(x+padding*2+renderWidth, y+renderHeight*2+(padding), renderWidth*3-padding*3, renderHeight-padding, roundedRadius);
+        noStroke();
+        fill(255);
+        text("Cash", x+padding+5, y+renderHeight*2+padding+20);
+        text(`${state.cash} / ${CASH_MAX}`, x+padding*2+renderWidth+5, y+renderHeight*2+padding+20);
+        
+    }
+
+    handleAction(action) {
+
+    }
+
+}
+
+//Displays how many stars you need to win, and how much wildness is currently activated. (3 to lose)
+class ConditionsScreen extends Screen {
+    constructor(state) {
+        super(state);
+    }
+
+    render(x, y, width, height, padding, roundedRadius) {
+        this.renderConditions(x, y, width, height, padding, roundedRadius);
+    }
+
+    renderConditions(x, y, width, height, padding, roundedRadius) {
+        // let renderHeight = height/2;
+        // let renderWidth = width;
+
+        // //Stars
+        // stroke(255);
+        // fill(0);
+        // rect(x+padding, y+padding, renderWidth, renderHeight-(padding), roundedRadius);
+
+        // //Wildness
+        // stroke(255);
+        // fill(0);
+        // rect(x+padding, y+renderHeight+padding, renderWidth, renderHeight-(padding), roundedRadius);
+        
+    }
+
+    handleAction(action) {
+
+    }
+
 }
